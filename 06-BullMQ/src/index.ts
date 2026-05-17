daad8382982fe4fc2b895b7b169bbe3fd6f03bde
amarnath0038 <amarnathreddy841@gmail.com>
2026-05-17 17:22:19 +0530

    clean up

--- 06-BullMQ/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { RedisClient } from "bun";
-import express from "express";
-
-const app = express();
-app.use(express.json());
-
-const redis = new RedisClient(process.env.REDIS_URL || "redis://localhost:6379");
-
-
